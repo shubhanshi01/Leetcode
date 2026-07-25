6d45b6b2fa435c8863572d2d51ed71d9337770e2
Shubhanshi Gupta <140481718+shubhanshi01@users.noreply.github.com>
2026-07-25 19:43:04 +0530

    Time: 90 ms (26.37%), Space: 99.6 MB (42.46%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+
+        double[][] cars = new double[n][2];
+
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = (double) (target - position[i]) / speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+        Stack<Double> stack = new Stack<>();
+
+        for (double[] car : cars) {
+            double currentTime = car[1];
+
+            if (stack.isEmpty() || currentTime > stack.peek()) {
+                stack.push(currentTime);
+            }
+        }
+
+        return stack.size();
+    }
+}
