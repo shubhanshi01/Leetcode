1af43ed6b64d17b695915db2a0a85bee7aa72f2b
Shubhanshi Gupta <140481718+shubhanshi01@users.noreply.github.com>
2026-07-31 07:07:47 +0530

    Time: 2 ms (96.37%), Space: 45.5 MB (92.54%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void  perm(int[] candidates, int target,int i, List<Integer>sum,List<List<Integer>>arr){
+
+        if(target==0){
+            arr.add(new ArrayList<>(sum));
+            return;
+        }
+        if(i==candidates.length ||target<0){
+            
+            return;
+        }
+
+        sum.add(candidates[i]);
+        perm(candidates,target-candidates[i],i,sum,arr);
+        sum.remove(sum.size()-1);
+        perm(candidates,target,i+1,sum,arr);
+
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> arr=new ArrayList<>();
+        List<Integer> sum=new ArrayList<>();
+        perm(candidates,target,0,sum,arr);
+
+        return arr;
+    }
+}
