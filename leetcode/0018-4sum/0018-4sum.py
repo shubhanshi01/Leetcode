4621898d00af136a88387a8cc60db13497c175f1
Shubhanshi Gupta <140481718+shubhanshi01@users.noreply.github.com>
2026-09-02 15:30:44 +0530

    Time: 17 ms (97.47%), Space: 19.2 MB (86.05%) - CodeHub

--- leetcode/0018-4sum/0018-4sum.py
+++ leetcode/0018-4sum/0018-4sum.py
@@ -1,37 +1,44 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n = len(nums)
+        n=len(nums)
         nums.sort()
-        count=defaultdict(int)
         res=[]
-
-        for num in nums:
-            count[num]+=1
-
-        for i in range(n):
-            count[nums[i]]-=1
-            if(i>0 and nums[i]==nums[i-1]):
+        if(n<4):
+            return []
+        for i in range(n-3):
+            if(i> 0 and nums[i]==nums[i-1]):
                 continue
-            for j in range(i+1,n):
-                count[nums[j]]-=1
-                if(j>i+1 and nums[j]==nums[j-1]):
+            if(nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target):
+                break
+            if(nums[i]+nums[n-1]+nums[n-2]+nums[n-3]<target):
+                continue
+            for j in range(i+1,n-2):
+                if(j> i+1 and nums[j]==nums[j-1]):
                     continue
-                for k in range(j+1,n):
-                    count[nums[k]]-=1
-                    if(k>j+1 and nums[k]==nums[k-1]):
-                    
-                        continue
-                    fourth=target-(nums[i]+nums[j]+nums[k])
-                    if(count[fourth]>0):
-                        res.append([nums[i],nums[j],nums[k],fourth])
-                for k in range(j+1,n):
-                    count[nums[k]]+=1
-            for j in range(i+1,n):
-                count[nums[j]]+=1
+                if(nums[i]+nums[j]+nums[j+1]+nums[j+2]>target):
+                    break
+                if(nums[i]+nums[j]+nums[n-1]+nums[n-2]<target):
+
+                    continue
+                l,r=j+1,n-1
+
+                while(l<r):
+                    total=nums[i]+nums[j]+nums[l]+nums[r]
+
+                    if(total==target):
+                        res.append([nums[i],nums[j],nums[l],nums[r]])
+                        l+=1
+                        r-=1
+                        while(l<r and nums[l]==nums[l-1]):
+                            l+=1
+                        while(r>l and nums[r]==nums[r+1]):
+                            r-=1
+                    elif(total<target):
+                        l+=1
+                    else:
+                        r-=1
         return res
-            
-            
+                    
                 
-
-
+                    
         
